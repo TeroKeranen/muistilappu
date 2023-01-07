24f050e144c812e3338c06022220d68a6d2064b2
Tero Keränen <tero-keranen@hotmail.com>
2023-01-07 08:20:29 +0200

    App.js alkuasetukset ja views kansion teko

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express(); 
+
+app.set('view engine', 'ejs'); // set ejs to project
+
+app.get("/", (req,res ) => {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    let day = today.toLocaleString("en-US", options)
+
+    res.render('list', {thisDay: day})
+
+})
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+})
