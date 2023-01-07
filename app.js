61b778d5ee71c95a88d5eb7f28a586b300dc71f1
Tero Keränen <tero-keranen@hotmail.com>
2023-01-07 08:39:01 +0200

    list.ejs proto, post formin lisäys, app.js testaus että tuote menee listalle ja näkyviin sivulle

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express(); 
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.set('view engine', 'ejs'); // set ejs to project
+
+let items = [];
 
 app.get("/", (req,res ) => {
 
@@ -17,7 +21,16 @@ app.get("/", (req,res ) => {
 
     let day = today.toLocaleString("en-US", options)
 
-    res.render('list', {thisDay: day})
+    res.render('list', {thisDay: day, newList: items})
+
+})
+
+app.post("/", (req,res) => {
+
+    item = req.body.newItem;
+    items.push(item);
+    
+    res.redirect('/');
 
 })
 
