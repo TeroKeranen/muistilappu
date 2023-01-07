a297af7e6c0208945129cc6976847df823064e6a
Tero Keränen <tero-keranen@hotmail.com>
2023-01-07 09:32:19 +0200

    ejs sivuston uuden tuotteen lisäys ja poista, app.js post ja delete POSTIT

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const app = express(); 
 
@@ -7,7 +8,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs'); // set ejs to project
 
-let items = [];
+mongoose.connect("mongodb+srv://testi:testi@cluster0.xecqchu.mongodb.net/muistilappu")
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema)
+
 
 app.get("/", (req,res ) => {
 
@@ -19,22 +27,55 @@ app.get("/", (req,res ) => {
         month: "long"
     }
 
+    
     let day = today.toLocaleString("en-US", options)
 
-    res.render('list', {thisDay: day, newList: items})
+
+    Item.find()
+        .then((result) => {
+            res.render('list', {thisDay:day, newItems:result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    
 
 })
 
 app.post("/", (req,res) => {
 
-    item = req.body.newItem;
-    items.push(item);
+    itemName = req.body.newItem;
     
-    res.redirect('/');
+    const item = new Item({
+        name: itemName
+    })
+    
+    item.save(function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/');
+        }
+    })
 
 })
 
+app.post("/delete", (req,res) => {
 
+    let deleteItem = req.body.checkbox;
+
+    Item.findByIdAndDelete(deleteItem, function(err) {
+        if (!err) {
+            console.log("Tuote poistettu listalta");
+            res.redirect("/");
+        } else {
+            console.log(err);
+        }
+
+    })
+    
+
+})
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
